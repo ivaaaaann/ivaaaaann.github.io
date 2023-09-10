08334a992b3e80b9e86e7c8531a95b9e057a3ad6
임동현 <79711744+ldh3907@users.noreply.github.com>
2023-09-11 00:39:31 +0900

    feat: prettier 적용, 메타 태그 적용, 레이아웃 구성

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const SITE_HOST = 'ivans-youth.vercel.app'
+export const SITE_URL = `https://${SITE_HOST}`;
+export const SITE_TITLE = "Ivan's Youth";
+export const SITE_DESCRIPTION = '안녕하세요! 저의 다사다난했던 청춘을 담아낸 블로그입니다. 주로 기술적 고찰과 회고 정도로 글을 올립니다.';
+
